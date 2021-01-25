4eb652b232757faa37da745b0f451715ac6d8e58
KeithTohx <keith2703@icloud.com>
2021-01-25 12:00:27 +0800

    added link to start page

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,8 @@ let acknowledge = document.getElementById('ackn');
 acknowledge.addEventListener('click', function() {
     window.location.href = "acknowledge.html";
 });
+
+let start = document.getElementById('start');
+start.addEventListener('click', function() {
+    window.location.href = "start.html";
+});
