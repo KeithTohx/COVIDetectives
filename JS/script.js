a8e43692220de1208d806875cdd0d76e20c7852a
KeithTohx <keith2703@icloud.com>
2021-01-25 12:49:15 +0800

    added typing effect

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,20 @@ let start = document.getElementById('start');
 start.addEventListener('click', function() {
     window.location.href = "start.html";
 });
+
+var disp = "It's great to see you, Detective!";
+function typing(disp,speed){
+    var char = 0;
+    var test = setInterval(type,speed)
+    function type(){
+        if (char <= disp.length){
+            var display = disp.slice(0,char);
+            $("#heading").text(display)
+            char++;
+            console.log(char)
+        }else{
+            clearInterval(test)
+        }
+    }
+}
+typing(disp,150)
