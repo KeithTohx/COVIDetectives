7d2d63455c2a4d7616ae2c2f234804f26eee98d1
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-01-31 22:15:28 +0800

    Add Checkpoint 1

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,56 @@
-let acknowledge = document.getElementById('ackn');
-acknowledge.addEventListener('click', function() {
-    window.location.href = "acknowledge.html";
+let acknowledge = document.getElementById("ackn");
+acknowledge.addEventListener("click", function () {
+  window.location.href = "acknowledge.html";
 });
 
-let start = document.getElementById('start');
-start.addEventListener('click', function() {
-    window.location.href = "start.html";
+let start = document.getElementById("start");
+start.addEventListener("click", function () {
+  window.location.href = "start.html";
 });
 
 var disp = "It's great to see you, Detective!";
-function typing(disp,speed){
-    var char = 0;
-    var test = setInterval(type,speed)
-    function type(){
-        if (char <= disp.length){
-            var display = disp.slice(0,char);
-            $("#heading").text(display)
-            char++;
-            console.log(char)
-        }else{
-            clearInterval(test)
-        }
+var dom = "#heading"
+function typing(disp, speed, dom) {
+  var char = 0;
+  var test = setInterval(type, speed);
+  function type() {
+    if (char <= disp.length) {
+      var display = disp.slice(0, char);
+      $(dom).text(display);
+      char++;
+      console.log(char);
+    } else {
+      clearInterval(test);
     }
+  }
 }
-typing(disp,150)
+typing(disp, 150,dom);
+
+//Checkpoint 1 onwards
 $(document).ready(function(){
     $("#checkpoint-1").hide();
 })
+function clue1() {
+  $("#startGame").hide();
+  $("#checkpoint-2").show();
+  $("#checkpoint-1 h2").css({ "margin-top": "100px" });
+  $("#checkpoint-1 h2,#checkpoint-1 div").css({ "text-align": "center" });
+  $("#clue1").css({
+    "border-style": "solid",
+    "border-width": "1px",
+    "border-color": "black",
+    "font-size": "20pt",
+    "margin-top": "50px",
+  });
+  $("#checkpoint-1 h3").css({ "text-align": "left" });
+  $("#clue1 li").css({ "text-align": "left" });
+  $("#clue1 ol").css({ "margin-left": "25px", padding: "0px" });
+  $("#clue1 section").css({ "margin-left": "145px" });
+  $("#clue1 section div").css({ "text-align": "left" });
+  $("#clue1-filler").css({ "margin-bottom": "100px" });
+  $("#map2Button").css({
+    "font-size": "12pt",
+    "margin-left": "70%",
+    "border-style": "none",
+  });
+}
