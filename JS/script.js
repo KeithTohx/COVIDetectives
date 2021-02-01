61f494dc8008308f395a8c1ae721543e955536c8
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-02-01 09:42:59 +0800

    Add map2Button

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+  $("#checkpoint-1").hide();
+})
+
 let acknowledge = document.getElementById("ackn");
 acknowledge.addEventListener("click", function () {
   window.location.href = "acknowledge.html";
@@ -27,33 +31,36 @@ function typing(disp, speed, dom) {
 typing(disp, 150,dom);
 
 //Checkpoint 1 onwards
-$(document).ready(function(){
-    $("#checkpoint-1").hide();
-})
 function clue1() {
-  $("#startGame").hide();
-  $("#checkpoint-1").show();
-  $("#checkpoint-1 h2").css({ "margin-top": "100px" });
-  $("#checkpoint-1 h2,#checkpoint-1 div").css({ "text-align": "center" });
-  $("#clue1").css({
-    "border-style": "solid",
-    "border-width": "1px",
-    "border-color": "black",
-    "font-size": "20pt",
-    "margin-top": "50px",
-  });
-  $("#checkpoint-1 h3").css({ "text-align": "left" });
-  $("#clue1 li").css({ "text-align": "left" });
-  $("#clue1 ol").css({ "margin-left": "25px", padding: "0px" });
-  $("#clue1 section").css({ "margin-left": "145px" });
-  $("#clue1 section div").css({ "text-align": "left" });
-  $("#clue1-filler").css({ "margin-bottom": "100px" });
-  $("#map2Button").css({
-    "font-size": "12pt",
-    "margin-left": "70%",
-    "border-style": "none",
-  });
-  let disp = "Eric Royal Lab Report"
-  let dom = "#clue1 h2"
-  typing(disp,150,dom)
+$("#startGame").hide();
+$("#checkpoint-1").show();
+$("#checkpoint-1 h2").css({ "margin-top": "100px" });
+$("#checkpoint-1 h2,#checkpoint-1 div").css({ "text-align": "center" });
+$("#clue1").css({
+  "border-style": "solid",
+  "border-width": "1px",
+  "border-color": "black",
+  "font-size": "20pt",
+  "margin-top": "50px",
+});
+$("#checkpoint-1 h3").css({ "text-align": "left" });
+$("#clue1 li").css({ "text-align": "left" });
+$("#clue1 ol").css({ "margin-left": "25px", padding: "0px" });
+$("#clue1 section").css({ "margin-left": "145px" });
+$("#clue1 section div").css({ "text-align": "left" });
+$("#clue1-filler").css({ "margin-bottom": "100px" });
+$("#map2Button").css({
+  "font-size": "12pt",
+  "margin-left": "70%",
+  "border-style": "none",
+});
+let disp = "Eric Royal Lab Report"
+let dom = "#clue1 h2"
+typing(disp,120,dom)
 }
+// transition to next show map
+$("#map2Button").click(function(){
+  $("#checkpoint-1").append(`<div id = map2></div>`)
+  $("#clue1").remove()
+})
+// Next function
