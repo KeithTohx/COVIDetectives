cde33373584dbac67dc5026a3de772563da46891
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-02-07 23:14:42 +0800

    Update scene 8

--- JS/script.js
+++ JS/script.js
@@ -773,7 +773,7 @@ function scene7() {
     });
   });
 }
-function scene8(){
+function scene8() {
   $("#scene7, #scene8Button").remove();
   $("#checkpoint-1").append(`<div id ="scene8">
   <div id="firstSentence"></div>
@@ -786,7 +786,7 @@ function scene8(){
   <div id="eightSentence"></div>
   </div>`);
   //Css styling
-  $("#checkpoint-1").css({"font-size":"18pt"})
+  $("#checkpoint-1").css({ "font-size": "18pt" });
   //The element selector
   let domFirst = "#firstSentence";
   let domSecond = "#secondSentence";
@@ -796,30 +796,32 @@ function scene8(){
   let domSixth = "#sixthSentence";
   let domSeven = "#sevenSentence";
   //Sentences
-  let firstSentence =`"Boom!"`
-  let secondSentence="The doors burst open!"
-  let thirdSentence = "Sir Suriv turn around with a look of surprise"
-  let fourthSentence = `"Well Well Well. Look who is here. Head Detective ${name}"`
-  let fifthSentence = `You snorted,"Freeze! By the way am I famous?"`
-  let sixthSentence = `"Surrender yourself now. You have the right to remain silent and refuse to answer questions. Anything you say may be used against you in a court of law. You have the right to consult an attorney before speaking to the police and to have an attorney present during questioning now or in the future."`
-  let seventhSentence = `He replied,"Your reputation precedes you. Ha Ha Ha"`
-  typingline(firstSentence,50,domFirst).then(function(){
-    typingline(secondSentence,50,domSecond).then(function(){
-      typingline(thirdSentence,50,domThird).then(function(){
-        typingline(fourthSentence,50,domFourth).then(function(){
-          typingline(fifthSentence,50,domFifth).then(function(){
-            typingline(sixthSentence,30,domSixth).then(function(){
-              typingline(seventhSentence,50,domSeven).then(function(){
-                typingline("           ", 400, "#eightSentence").then(function(){
-                  preEncounter()
-                })
-              })
-            })
-          })
-        })
-      })
-    })
-  })
+  let firstSentence = `"Boom!"`;
+  let secondSentence = "The doors burst open!";
+  let thirdSentence = "Sir Suriv turn around with a look of surprise";
+  let fourthSentence = `"Well Well Well. Look who is here. Head Detective ${name}"`;
+  let fifthSentence = `You snorted,"Freeze! By the way am I famous?"`;
+  let sixthSentence = `"Surrender yourself now. You have the right to remain silent and refuse to answer questions. Anything you say may be used against you in a court of law. You have the right to consult an attorney before speaking to the police and to have an attorney present during questioning now or in the future."`;
+  let seventhSentence = `He replied,"Your reputation precedes you. Ha Ha Ha"`;
+  typingline(firstSentence, 50, domFirst).then(function () {
+    typingline(secondSentence, 50, domSecond).then(function () {
+      typingline(thirdSentence, 50, domThird).then(function () {
+        typingline(fourthSentence, 50, domFourth).then(function () {
+          typingline(fifthSentence, 50, domFifth).then(function () {
+            typingline(sixthSentence, 30, domSixth).then(function () {
+              typingline(seventhSentence, 50, domSeven).then(function () {
+                typingline("           ", 400, "#eightSentence").then(
+                  function () {
+                    preEncounter();
+                  }
+                );
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 }
 function preEncounter(){
   $("#scene8").remove();
