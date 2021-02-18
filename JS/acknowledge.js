96d8b236b38e5a6a0770645b68bb5792b83e779b
KeithTohx <keith2703@icloud.com>
2021-02-18 17:29:01 +0800

    added comment

--- JS/acknowledge.js
+++ JS/acknowledge.js
@@ -1,3 +1,4 @@
+// bring players back to home page after clicking on back button
 let homePage = document.getElementById('back');
 homePage.addEventListener('click', function() {
     window.location.href = "index.html";
